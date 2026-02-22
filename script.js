fe3dc1667af5fc7a0281c024f321422b74b85e83
shimfevictor978-dev <shimfevictor978@gmail.com>
2026-02-22 01:05:10 +0100

    Confetti & Responsiveness

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             progressNumbers.textContent = `${completedTasks} / ${totalTasks}`;
 
+
+            if (checkCompletion && totalTasks > 0 && completedTasks === totalTasks){
+                Confetti();
+            }
+
         }  
 
 
@@ -109,3 +114,37 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 })
+
+
+
+const Confetti = () => {
+    const defaults = {
+        spread: 360,
+        ticks: 50,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["star"],
+        colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+        };
+
+    function shoot() {
+        confetti({
+            ...defaults,
+            particleCount: 40,
+            scalar: 1.2,
+            shapes: ["star"],
+        });
+
+        confetti({
+            ...defaults,
+            particleCount: 10,
+            scalar: 0.75,
+            shapes: ["circle"],
+        });
+        }
+
+        setTimeout(shoot, 0);
+        setTimeout(shoot, 100);
+        setTimeout(shoot, 200);
+}
